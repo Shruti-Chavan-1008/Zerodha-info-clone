5548ba929713dd2a8a2b391e19d6cf5b8e6ba54c
Shruti-Chavan-1008 <shrutichavan1975@gmail.com>
2026-07-05 02:06:23 +0530

    All component of the UI is alreday have main file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
 import HomePage from './landing_page/Home/HomePage';
- 
+import { BrowserRouter,Routes,Route } from 'react-router-dom'; 
+import Pricing from './landing_page/Pricing/PricingPage';
+import Support from './landing_page/Support/Support';
+import Product from './landing_page/Products/Product';
+import Singup from './landing_page/Singup/singup';
+import AboutPage from './landing_page/About/AboutPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-      <HomePage/> 
-    
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/AboutPage" element={<AboutPage/>}></Route>
+     <Route path='/pricing' element={<Pricing/>}></Route>
+     <Route path='/product' element={<Product/>}></Route>
+      <Route path='/Singup' element={<Singup/>}></Route>
+       <Route path='/support' element={<Support/>}></Route>
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
 );
 
  
