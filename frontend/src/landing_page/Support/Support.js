6c06149ad59ca4ea0ea3e0efe3e85a178b7ae2c1
Shruti-Chavan-1008 <shrutichavan1975@gmail.com>
2026-07-06 00:09:36 +0530

    support page  is done

--- frontend/src/landing_page/Support/Support.js
+++ frontend/src/landing_page/Support/Support.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Navbar from '../Navbar';
 import Hero from './hero';
 import Ticket from './Ticket';
+import Footer from '../Footer';
 function Support() {
     return (
          <>
-         <Navbar/>
+       
          <Hero/>
          <Ticket/>
-         <Footer/>
+          
          </>
       );
 }
--- frontend/src/landing_page/Support/hero.js
+++ frontend/src/landing_page/Support/hero.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Hero() {
-    return ( 
-        <h1>hero</h1>
-     );
+  return (
+     <div className="row" style={{backgroundColor:"#425df4",color:"white"}}>
+        <div className="col-1"></div>
+         
+         <div className="col"  >
+            <div style={{marginBottom:"50px", marginTop:"20px"}} > 
+         <h4  >Support Portal</h4>
+         <Link style={{color:"white",marginLeft:"1000px"}}>Track tickect</Link>
+         </div>
+         <div style={{display:"flex",gap:"200px",marginBottom:"100px"}}> 
+          <div >
+            <h4>
+              Search for an answer or browser help topic to create a tickect
+            </h4>  
+             <input type="text" class="form-control" placeholder="Eg. how do activate F&o ,why is my order geeting rejected " aria-label="Username" aria-describedby="inputGroup-sizing-lg"></input>
+              
+             <Link style={{color:"white",padding:"2px"}}>Track account opening</Link>
+             <Link  style={{color:"white",padding:"2px"}}> track segment activation </Link>
+             <Link  style={{color:"white",padding:"2px"}}> Interday margin</Link>
+             <Link  style={{color:"white",padding:"2px"}}>kite user munnal</Link>
+             
+          </div>
+          <div >
+            <h4>Featured</h4>
+             1. <Link style={{color:"white"}}>Current Take Overs and Delisting -January 2026</Link> <br />
+             2. <Link style={{color:"white"}}>Letest INterday leverages -MIS & CO</Link> <br />
+          </div>
+           </div>
+          </div>
+          <div className="col-1"></div>
+          </div>
+  );
 }
 
 export default Hero;
